cd1ce6ff229b68450ee8e617991a1a4cc8ab1a07
Petey <thatskinnywhiteguy@gmail.com>
2013-02-23 20:02:38 -0700

    added modified GPL2

--- machine.py
+++ machine.py
@@ -1,3 +1,28 @@
+# TODO
+# one line to give the program's name and an idea of what it does.
+# Copyright (C) 2013  Petey Aldous
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# *ADDITIONAL TERMS*
+# This software or binaries made from this software may not be
+# distributed with any form digital rights management or any
+# advertisements. Similarly, no software that uses or is
+# protected by digital rights management or that displays,
+# stores, or transmits advertisements in any form may make use of
+# this software.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import subprocess
 
 class Command:
